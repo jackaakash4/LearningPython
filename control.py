85b639fc6327c70681f313454670328094bc65c7
Jack Aakash <jackaakash4@gmail.com>
2025-08-04 17:24:17 +0545

    Learned: variable, control flow and loops
    datatypes is learned i.e. int, float, string and boolean. Input and
    output is learned. if control statement is learned and found the
    importance of identation in python and last but not least learned for
    loop

---
+++ control.py
@@ -0,0 +1,27 @@
+#variables and data types
+
+x = 10
+y = 3.14
+name = "Aakash"
+is_married = False
+
+#Input and Output
+
+user_input = input("Enter something: ")
+print("You've entered ", user_input)
+
+#control flow
+
+if x > 5:
+    print("x is greater than 5")
+elif x == 5:
+    print("x is 5")
+else: 
+    print("x is less than 5")
+
+#loops
+
+for i in range(5):
+    print(i)
+    x -= 1
+    
